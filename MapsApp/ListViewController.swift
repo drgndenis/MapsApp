49214863c5b4e258834fdfa9e9b4688e56324267
Denis Dragan <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-06-05 18:55:03 +0300

    Added TableView and CoreData

---
+++ MapsApp/ListViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ListViewController.swift
+//  MapsApp
+//
+//  Created by Denis DRAGAN on 5.06.2023.
+//
+
+import UIKit
+import CoreData
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+    
+    var nameArrays = [String]()
+    var idArrays = [UUID]()
+    var selectedLocName = ""
+    var selectedLocId : UUID?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addLocation))
+        
+        getData()
+    }
+    
+    @objc func addLocation() {
+        selectedLocName = "" // Navbar'dan butona tiklandiginda bos veri gonderiyoruz ve MapsView uzerinde ekleme yapilacak
+        performSegue(withIdentifier: "toMapsVC", sender: nil)
+    }
+    
+    func getData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Locations")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                nameArrays.removeAll(keepingCapacity: false)
+                idArrays.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String {
+                        nameArrays.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idArrays.append(id)
+                    }
+                }
+                tableView.reloadData()
+            }
+        } catch {
+            print("Error")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArrays.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArrays[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedLocName = nameArrays[indexPath.row]
+        selectedLocId = idArrays[indexPath.row]
+        performSegue(withIdentifier: "toMapsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toMapsVC" {
+            let destinationVC = segue.destination as! MapsViewController
+            destinationVC.selectedName = selectedLocName
+            destinationVC.selectedId = selectedLocId
+        }
+    }
+}
---
+++ MapsApp/MapsViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  MapsApp
+//
+//  Created by Denis DRAGAN on 27.05.2023.
+//
+
+import UIKit
+import CoreData
+import MapKit
+import CoreLocation // get the users location
+
+class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var nameTextFields: UITextField!
+    @IBOutlet var notesTextField: UITextField!
+    
+    var locationManager = CLLocationManager()
+    var selectedLatitude = Double()
+    var selectedLongitude = Double()
+    var selectedName = ""
+    var selectedId: UUID?
+    var annotationTitle = ""
+    var annotationSubtitle = ""
+    var annotationLatitude =  Double()
+    var annotationLongitude = Double()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // Konumu mesafe
+        locationManager.requestWhenInUseAuthorization() // Uygulamayi kullanirken konumun kullanilmasina izin verilmesi
+        locationManager.startUpdatingLocation() // Konumun guncellenmesi
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(selectLocation))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
+        if selectedName != "" {
+            // Core Data'dan verileri cek
+            if let uuidString = selectedId?.uuidString {
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                let context = appDelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Locations")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+                
+                do {
+                    let results = try context.fetch(fetchRequest)
+                    
+                    if results.count > 0 {
+                        for result in results as! [NSManagedObject] {
+                            
+                            if let name = result.value(forKey: "name") as? String {
+                                annotationTitle = name
+                                if let note = result.value(forKey: "note") as? String {
+                                    annotationSubtitle = note
+                                    if let latitude = result.value(forKey: "latitude") as? Double {
+                                        annotationLatitude = latitude
+                                        if let longitude = result.value(forKey: "longitude") as? Double {
+                                            annotationLongitude = longitude
+                                            // Pin ekleme
+                                            let annotation = MKPointAnnotation()
+                                            annotation.title = annotationTitle
+                                            annotation.subtitle = annotationSubtitle
+                                            
+                                            let coordinate = CLLocationCoordinate2D(latitude: annotationLatitude, longitude: annotationLongitude)
+                                            annotation.coordinate = coordinate
+                                            
+                                            mapView.addAnnotation(annotation)
+                                            nameTextFields.text = annotationTitle
+                                            notesTextField.text = annotationSubtitle
+                                            
+                                            locationManager.stopUpdatingLocation() // Konum guncellemesi durdur
+                                            
+                                            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                            let region = MKCoordinateRegion(center: coordinate, span: span)
+                                            mapView.setRegion(region, animated: true)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch {
+                    print("Error")
+                }
+            }
+        } else {
+            // Veri ekleme
+        }
+    }
+    
+    
+    @objc func selectLocation(gestureRecognizer : UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let touchedLocation = gestureRecognizer.location(in: mapView)
+            let touchedCoordinate  = mapView.convert(touchedLocation, toCoordinateFrom: mapView)
+            selectedLatitude = touchedCoordinate.latitude
+            selectedLongitude = touchedCoordinate.longitude
+            
+            // Pin ekleme
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinate
+            annotation.title = nameTextFields.text
+            annotation.subtitle = notesTextField.text
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations[0].coordinate.latitude)  // Enlem oranlari
+        print(locations[0].coordinate.longitude)  // Boylam oranlari
+        if selectedName == "" {
+            let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05) // Harita acildiginda var olan konumun ne kadar yakin veya uzak gosterecegi
+            let region = MKCoordinateRegion(center: location, span: span) // Konum degisikligi
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    @IBAction func savePressed(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newLocation = NSEntityDescription.insertNewObject(forEntityName: "Locations", into: context)
+        newLocation.setValue(nameTextFields.text, forKey: "name")
+        newLocation.setValue(notesTextField.text, forKey: "note")
+        newLocation.setValue(selectedLatitude, forKey: "latitude")
+        newLocation.setValue(selectedLongitude, forKey: "longitude")
+        newLocation.setValue(UUID(), forKey: "id")
+        
+        do {
+            try context.save()
+            print("Saved")
+        } catch {
+            print("Error")
+        }
+    }
+}
